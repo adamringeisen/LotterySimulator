11071b030ad3127045d2e009e649f545f484cfe9
Adam Ringeisen <ringeisen@gmail.com>
2022-04-20 16:03:26 -0400

    Removed old commented out code

--- LotterySimulator/Global.cs
+++ LotterySimulator/Global.cs
@@ -1,24 +1,4 @@
-﻿//static void GetInput()
-//{
-//    Console.WriteLine("How many tickets do you want to buy each week?");
-//    List<Roll> tickets = getTickets(Console.ReadLine());
-//    for (int i = 0; i < tickets.Count; i++)
-//    {
-//        Console.WriteLine(tickets[i].);
-//    }
-
-//    Console.SetCursorPosition(0,0);
-//    Console.WriteLine("How many weeks do you want to run the simulation for?");
-//    string weekans = Console.ReadLine();
-
-//}
-
-
-
-// Ask how many tickets each week
-// Ask how many years to simulate
-//
-public static class Global
+﻿public static class Global
 {
     public static Random rand = new Random();
 }
